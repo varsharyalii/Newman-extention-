e61a6c7d586966e791fef174488eaa749a3915c8
MrEgyptian <Me@MrEgyptian.com>
2022-11-10 00:36:13 +0200

    lil changes

--- tagging.sh
+++ tagging.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
-tag=$1
+#!/bin/bash
+optparse(){
+while getopts "e:c:" opt; do
+  case "$opt" in
+    e)
+    environment=$OPTARG
+    ;;
+    c)
+    collection=$OPTARG
+    ;;
+  esac
+done
+}
+
+tagging(){
 #-c collection
 #-e env
-newman run <(cat Cosmos.postman_collection.json | jq -c '
+newman run <(cat $collection | jq -c '
       del(..| select(
      (.request)?
      and (
         (.name)? | test("\\['${tag}'\\]") | not
         )
      ))
- ')  --environment cosmos.postman_environment.json
+ ')  --environment $environment
+}
+if [[ -z $1 ]]; then
+ echo -e 'No enough args :('
+ exit 1
+else
+ environment='cosmos.postman_environment.json'
+ collection='Cosmos.postman_collection.json'
+ optparse $@
+ shift $(( OPTIND - 1 ))
+ echo $@
+ tagging $@
+fi
